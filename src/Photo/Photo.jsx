34f9af08c3282f1756d4f3fad44dd08e422e7567
estevamnetof <estevam.neto07@gmail.com>
2023-08-29 00:56:34 -0300

    🛠 head das páginas

--- src/Photo/Photo.jsx
+++ src/Photo/Photo.jsx
@@ -5,6 +5,7 @@ import { PHOTO_GET } from '../api';
 import Error from '../Components/Helper/Error';
 import Loading from '../Components/Helper/Loading';
 import PhotoContent from './PhotoContent';
+import Head from '../Components/Helper/Head';
 
 const Photo = () => {
   const { id } = useParams();
@@ -20,6 +21,7 @@ const Photo = () => {
   if (data)
     return (
       <section className="container mainContainer">
+        <Head title={data.photo.title} />
         <PhotoContent single={true} data={data} />
       </section>
     );
